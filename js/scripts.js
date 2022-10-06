2bff293e5cbd8992b7225878fe1f9b66c0779d72
Luis-Filipe-Santos <109242677+Luis-Filipe-Santos@users.noreply.github.com>
2022-10-06 17:26:11 -0300

    f

--- js/scripts.js
+++ js/scripts.js
@@ -77,10 +77,10 @@ function checkWinCondition() {
 
         if (b1Child == 'x' && b2Child == 'x' && b3Child == 'x') {
             // x
-            console.log('x venceu');
+            declareWinner('x');
         } else if(b1Child == 'o' && b2Child == 'o' && b3Child == 'o') {
             // o
-            console.log('o venceu');
+            declareWinner('o');
         }
     }
 
@@ -92,10 +92,10 @@ function checkWinCondition() {
 
         if (b4Child == 'x' && b5Child == 'x' && b6Child == 'x') {
             // x
-            console.log('x venceu');
+            declareWinner('x');
         } else if(b4Child == 'o' && b5Child == 'o' && b6Child == 'o') {
             // o
-            console.log('o venceu');
+            declareWinner('o');
         }
     }
 
@@ -107,10 +107,10 @@ function checkWinCondition() {
 
         if (b7Child == 'x' && b8Child == 'x' && b9Child == 'x') {
             // x
-            console.log('x venceu');
+            declareWinner('x');
         } else if(b7Child == 'o' && b8Child == 'o' && b9Child == 'o') {
             // o
-            console.log('o venceu');
+            declareWinner('o');
         }
     }
 
@@ -124,10 +124,10 @@ function checkWinCondition() {
 
         if (b1Child == 'x' && b4Child == 'x' && b7Child == 'x') {
             // x
-            console.log('x venceu');
+            declareWinner('x');
         } else if(b1Child == 'o' && b4Child == 'o' && b7Child == 'o') {
             // o
-            console.log('o venceu');
+            declareWinner('o');
         }
     }
 
@@ -139,10 +139,10 @@ function checkWinCondition() {
 
         if (b2Child == 'x' && b5Child == 'x' && b8Child == 'x') {
             // x
-            console.log('x venceu');
+            declareWinner('x');
         } else if(b2Child == 'o' && b5Child == 'o' && b8Child == 'o') {
             // o
-            console.log('o venceu');
+            declareWinner('o');
         }
     }
 
@@ -154,10 +154,10 @@ function checkWinCondition() {
 
         if (b3Child == 'x' && b6Child == 'x' && b9Child == 'x') {
             // x
-            console.log('x venceu');
+            declareWinner('x');
         } else if(b3Child == 'o' && b6Child == 'o' && b9Child == 'o') {
             // o
-            console.log('o venceu');
+            declareWinner('o');
         }
     }
 
@@ -171,10 +171,10 @@ function checkWinCondition() {
 
         if (b1Child == 'x' && b5Child == 'x' && b9Child == 'x') {
             // x
-            console.log('x venceu');
+            declareWinner('x');
         } else if(b1Child == 'o' && b5Child == 'o' && b9Child == 'o') {
             // o
-            console.log('o venceu');
+            declareWinner('o');
         }
     }
 
@@ -186,10 +186,10 @@ function checkWinCondition() {
 
         if (b7Child == 'x' && b5Child == 'x' && b3Child == 'x') {
             // x
-            console.log('x venceu');
+            declareWinner('x');
         } else if(b7Child == 'o' && b5Child == 'o' && b3Child == 'o') {
             // o
-            console.log('o venceu');
+            declareWinner('o');
         }
     }
 
@@ -205,7 +205,30 @@ function checkWinCondition() {
     }
 
     if (counter == 9) {
-        console.log('deu velha');
+        declareWinner('Deu velha!')
     }
+
+}
+
+// limpa o jogo, declara o vencedor e atualiza o placar
+function declareWinner(winner) {
+    
+    let scoreboardX = document.querySelector('#scoreboard-1');
+    let scoreboardy = document.querySelector('#scoreboard-2');
+    let msg = '';
+
+    if(winner == 'x'){
+        scoreboardX.textContent = parseInt(scoreboardX.textContent) + 1;
+        msg = "O jogador 1 venceu!";
+    } else if(winner == 'y') {
+        scoreboardy.textContent = parseInt(scoreboardy.textContent) + 1;
+        msg = "O jogador 2 venceu!";
+    } else {
+        msg = "Deu velha!"
+    }
+
+    // exibir msg
+    messageText.innerHTML = msg;
+    messageContainer.classList.remove("hide");
 
 }
